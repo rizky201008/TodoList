50b08e8c29ddfad3539ca1d369915702066d3344
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-06-13 17:17:29 +0700

    Update

---
+++ app/src/main/java/com/tugasakhir/todolist/AddActivity.java
@@ -0,0 +1,17 @@
+package com.tugasakhir.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
--- app/src/main/java/com/tugasakhir/todolist/MainActivity.java
+++ app/src/main/java/com/tugasakhir/todolist/MainActivity.java
@@ -2,13 +2,25 @@ package com.tugasakhir.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView btn_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_add = findViewById(R.id.fab);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
+            }
+        });
     }
 }
