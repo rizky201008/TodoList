d0ff08f15b0abc81afabbe4d11b746d38c910926
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2022-06-23 12:56:55 +0700

    Update AddActivity.java

--- app/src/main/java/com/tugasakhir/todolist/AddActivity.java
+++ app/src/main/java/com/tugasakhir/todolist/AddActivity.java
@@ -2,6 +2,7 @@ package com.tugasakhir.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
@@ -9,9 +10,18 @@ import android.widget.ImageView;
 
 public class AddActivity extends AppCompatActivity {
 
+    ImageView btn_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        btn_back = findViewById(R.id.back_btn);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
